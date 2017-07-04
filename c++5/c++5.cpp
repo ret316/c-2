bae6fc56c60c328dae460979b06470f201214a14
ret316 <ret316@yandex.ru>
2017-07-04 10:36:58 +0400

    c++5/2

--- c++5/c++5.cpp
+++ c++5/c++5.cpp
@@ -13,13 +13,25 @@ int main()
 		"Ахмедов", "Ерошкин", "Выхин",
 		"Андеев", "Вин Дизель", "Картошкин", "Чубайс"
 	};
-
 	for (int i = 0; i < (end(students) - begin(students)) ; i++)
 	{
 		std::cout << students[i] << std::endl;
 	}
 
+	cout << "\n\n";
+
+	int arr[10];
+	for (int j = 0; j < 10; j++) {
+		cout << "[" << j + 1 << "]" << ": ";
+		cin >> arr[j];
+	}
+	cout << "\nВаш массив: ";
+	for (int j = 0; j < 10; ++j) {
+		cout << arr[j] << " ";
+	}
+	cout << endl;
 	cin.get();
+	system("pause");
     return 0;
 }
 
