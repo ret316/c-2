c8f52c6f74e16c9afc73efe51396e37ed020437b
ret316 <ret316@yandex.ru>
2017-07-10 00:11:04 +0400

    c++6/2-end

--- c++6/c++6.cpp
+++ c++6/c++6.cpp
@@ -11,6 +11,29 @@ void hello(string hello)
 	cout << "Здравствуйте " << hello << endl;
 }
 
+bool password_is_valid(string password)
+{
+	string valid_pass = "qwerty123";
+	if (valid_pass == password)
+		return true;
+	else
+		return false;
+}
+
+void get_pass()
+{
+	string user_pass;
+	cout << "Введите пароль: ";
+	getline(cin, user_pass);
+	if (!password_is_valid(user_pass)) {
+		cout << "Неверный пароль!" << endl;
+		get_pass();
+	}
+	else {
+		cout << "Доступ разрешен." << endl;
+	}
+}
+
 int main()
 {
 	string name;
@@ -19,6 +42,7 @@ int main()
 	getline(cin, name);
 	cout << endl;
 	hello(name);
+	get_pass();
 	cin.get();
     return 0;
 }
