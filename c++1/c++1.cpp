72c2d52a5aad856782a4e30588467a4e5d70a50d
ret316 <ret316@yandex.ru>
2017-07-19 11:36:12 +0400

    forgot 1st lesson

---
+++ c++1/c++1.cpp
@@ -0,0 +1,14 @@
+// c++1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+	system("pause");
+	return 0;
+}
